339d053d06e0a339c03276d0020a18aa801f75d8
Donald Kelly <donaldkelly0@gmail.com>
2023-11-10 23:40:32 -0500

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,45 @@
 import React from 'react'
-import './Cards.css';
 import CardItem from './CardItem';
+import './Cards.css';
+
 
 function Cards() {
   return (
     <div className='cards'>
-        <h1> Check out these perfumes! </h1>
+        <h1> Check out our newest items! </h1>
         <div className ="cards__container">
             <div className="cards__wrapper">
                 <ul className="cards__items">
-                    <CardItem/>
+                    <CardItem
+                    src="ReactPictures/feast.jpg"
+                    text = "Explore our newest Fragrences"
+                    label = 'Scents'
+                    path = '/frangrences'
+                    />
+
+                    <CardItem
+                    src="ReactPictures/Jewlery-Necklace.jpg"
+                    text = "Explore our newest Jewlery"
+                    label = 'Jewlery'
+                    path = '/frangrences'
+                    />
+
+                      <CardItem
+                    src="ReactPictures/stock-photo.jpg"
+                    text = "Explore our newest Fragrences"
+                    label = 'Clothes'
+                    path = '/frangrences'
+                    />
+                    
+                    <CardItem
+                    src="ReactPictures/HandModel-Perfume.jpg"
+                    text = "Explore our newest Fragrences"
+                    label = 'Reward Program'
+                    path = '/frangrences'
+                    />
                 </ul>
-
             </div>
-
         </div>
-
     </div>
   )
 }
